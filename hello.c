f274f675e77ccfa40c5a5072bbf8002fab1d139f
cristofer sanchez <csanchez@huskers.unl.edu>
2022-08-25 19:35:09 +0000

    intial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Cristofer Sanchez
+ *         csanchez13@huskers.unl.edu
+ * Date: 2022/08/25
+ *
+ * a simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argr, char **argv) {
+
+    printf("Cristofer Sanchez\n") ;
+    return 0;
+}
